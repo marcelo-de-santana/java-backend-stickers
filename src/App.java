03522f61d35f7a4984d2258053b4263a2fb83680
marcelo-de-santana <marcelondesantana@outlook.com>
2022-07-19 01:22:47 -0300

    Aula 01 - Primeira Parte

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.net.http.HttpClient;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //fazer uma conexão HTTP  e buscar os top 250 filmes
+        //String url = "https://alura-filmes.herokuapp.com/conteudos";
+        //HttpClient cliente = HttpClient.newHttpClient();
+        //48:30
+        //extrair só os dados que interessam (título, poster, classificação)
+
+        //exibir e manipular os dados 
+    }
+}
